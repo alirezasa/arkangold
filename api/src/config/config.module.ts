1783cce6776f87c17e3ab5bac2618d9164154ce2
Alirezasadeghzadeh <alirezasadeghzadeh98@gmail.com>
2026-06-13 17:26:10 +0330

    up 20

--- api/src/config/config.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { SystemConfigService } from './system-config.service';
-
-@Global()
-@Module({
-  providers: [SystemConfigService],
-  exports: [SystemConfigService],
-})
-export class ConfigModules {}

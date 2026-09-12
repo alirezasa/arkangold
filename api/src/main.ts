134cd9d8ed8e08b7a680455904776fead9b41a78
Alirezasadeghzadeh <alirezasadeghzadeh98@gmail.com>
2026-09-12 15:05:15 +0330

    up deposite

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,37 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // ─── فایل‌های تصویر محصولات — عمومی و بدون نیاز به توکن ───
+  // فایل‌های تصویر محصولات
   app.useStaticAssets(join(process.cwd(), 'uploads', 'products'), {
     prefix: '/uploads/products',
   });
 
-  // ─── تنظیمات CORS برای ارتباط با Next.js ───
+  // تنظیمات CORS
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'idempotency-key',
+    ],
   });
-  // ──────────────────────────────────────────
 
-  // ── اضافه کردن Swagger ──
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('آرکان گلد')
     .setDescription('مستندات API')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api/docs', app, document);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // اعتبارسنجی و تبدیل DTOها
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(5000);
 }
+
 void bootstrap();
